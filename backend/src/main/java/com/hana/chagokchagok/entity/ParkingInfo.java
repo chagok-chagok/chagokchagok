ac364bd16ab3ba83ea058900d3ac9331d42099e5
김용준 <rladydwns129@naver.com>
2024-01-27 10:31:26 +0900

    :art: ParkingInfo Class에 주차 자리 풀네임 반환 메소드 생성
    Change-Id: I9ac500e887eb71648c57146539cb43e52f26eeb9

--- backend/src/main/java/com/hana/chagokchagok/entity/ParkingInfo.java
+++ backend/src/main/java/com/hana/chagokchagok/entity/ParkingInfo.java
@@ -21,4 +21,14 @@ public class ParkingInfo {
     private Integer parkNo;
     @Column(name = "is_disabled")
     private Boolean isDisabled;
+
+    /**
+     * parkNo가 areaCode, parkNo로 분리됨에 따라
+     * 기존 parkNo 형태로 리턴하는 메소드
+     * @author 김용준
+     * @return 구역코드 + 자리번호 (ex) A01
+     */
+    public String getFullName() {
+        return this.areaCode + this.parkNo;
+    }
 }
