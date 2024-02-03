362e09b5637cac307f5b7b6b4be132134821546f
최서현 <cheshire99.dev@gmail.com>
2024-02-03 19:00:58 +0900

    :sparkles: navbar 링크 연결
    Change-Id: I6859e0e054a0b13903280fb21db463b62234ebbd

--- frontend/chagokchagok/src/views/admin/DashBoard.vue
+++ frontend/chagokchagok/src/views/admin/DashBoard.vue
@@ -362,7 +362,7 @@ p {
   display: flex;
   flex-direction: column;
   /* background-color: rgb(156, 151, 211); */
-  border: 1px solid black;
+  /* border: 1px solid black; */
   width: 100%;
   height: 100%;
   gap: 0%;
