3e34cf2ebf9c956b156bfefcc57c544dc2ecb795
김용준 <rladydwns129@naver.com>
2024-02-05 12:08:20 +0900

    :ambulance: ValidationResponse 리턴값 String으로 변경
    Change-Id: I7998458306f8d5299a9e2b108e2aa92da9fe6fbc

--- backend/src/main/java/com/hana/chagokchagok/dto/ValidationParkingInfoDto.java
+++ backend/src/main/java/com/hana/chagokchagok/dto/ValidationParkingInfoDto.java
@@ -11,4 +11,8 @@ import lombok.ToString;
 public class ValidationParkingInfoDto {
     private Integer parkId; // 주차장 번호
     private String areaCode; // 구역 코드
+    public String getFullName() {
+        if (this.parkId == null || this.areaCode == null) return null;
+        return this.areaCode + this.parkId;
+    }
 }
