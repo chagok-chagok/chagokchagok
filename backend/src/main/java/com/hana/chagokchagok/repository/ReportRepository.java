b2f13f8ae490c05f756e40e429312e2bd9a52ca1
최서현 <cheshire99.dev@gmail.com>
2024-02-07 21:52:31 +0900

    :lipstick: 기타 CSS 수정
    Change-Id: I5d4d4a30714aa7cf887c4c776d3ccd119d84b428

--- backend/src/main/java/com/hana/chagokchagok/repository/ReportRepository.java
+++ backend/src/main/java/com/hana/chagokchagok/repository/ReportRepository.java
@@ -14,7 +14,7 @@ public interface ReportRepository extends JpaRepository<Report, Long> {
     Page<Report> findAll(Pageable pageable); // 현재 페이지 글 목록 조회
     List<Report> findAllByReportTimeBetween(LocalDateTime startOfDay, LocalDateTime endOfDay); // 오늘 날짜 신고 목록 조회
 
-    List<Report> findTop5ByOrderByDoneTimeDesc(); //최근 처리완료된 5개 데이터를 조회
+    List<Report> findTop4ByOrderByDoneTimeDesc(); //최근 처리완료된 5개 데이터를 조회
     // findById를 @Override하면 순환 참조때문에 오류가 생김
     Report findByReportId(Long reportId); // reportId로 신고 조회
 
