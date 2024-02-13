65c6d56648b4ec27dda9e282562984f6081e5b51
백성욱 <b9s2w6@naver.com>
2024-02-14 04:51:23 +0900

    :construction_worker: 자동 빌드 테스트
    Change-Id: I6283fce41f73c18a96e7f9ae84e34a1ca9d3c5d6

--- frontend/chagokchagok/src/stores/dashboard.js
+++ frontend/chagokchagok/src/stores/dashboard.js
@@ -14,7 +14,6 @@ export const dashBoardStore = defineStore("dashBoardStore", () => {
     getDashBoard,
   };
 });
-
 // import { ref } from "vue";
 // import { defineStore } from "pinia";
 // import axios from "axios";
