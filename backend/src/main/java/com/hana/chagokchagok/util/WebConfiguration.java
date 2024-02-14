96baa31cec2a7396443cba8de4ecdbf7d90715ae
최서현 <cheshire99.dev@gmail.com>
2024-02-14 16:41:50 +0900

    :ambulance: 장고에 입구차단바 해제요청 서비스 추가
    Change-Id: Ia6e15d21b020471b0a85e330cb480b0aa25ffda1

--- backend/src/main/java/com/hana/chagokchagok/util/WebConfiguration.java
+++ backend/src/main/java/com/hana/chagokchagok/util/WebConfiguration.java
@@ -40,7 +40,7 @@ public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(jwtInterceptor).excludePathPatterns("/admin/login", "/sse/admin");
+        registry.addInterceptor(jwtInterceptor).excludePathPatterns("/admin/login", "/sse/admin", "/park/**");
     }
 
 }
