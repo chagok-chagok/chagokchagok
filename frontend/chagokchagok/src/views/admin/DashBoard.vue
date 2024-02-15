ee5f2a749ecf595b6d1d1b4d2ab4f2ec64f68fb6
최서현 <cheshire99.dev@gmail.com>
2024-02-15 10:24:00 +0900

    :art: 대시보드 표 fontsize 변경
    Change-Id: I56f20f9719bfd796d9f36c5aa50f5730f1784249

--- frontend/chagokchagok/src/views/admin/DashBoard.vue
+++ frontend/chagokchagok/src/views/admin/DashBoard.vue
@@ -428,6 +428,7 @@ p {
   /* background-color: aliceblue; */
   height: 45%;
   width: 90%;
+  font-size: 0.7rem;
 }
 
 .horizontal-line {
@@ -483,7 +484,7 @@ p {
 table {
   width: 100%;
   border-collapse: collapse;
-  font-size: 14px; /* 원하는 폰트 크기로 조절 */
+  /* font-size: 14px; */
 }
 th,
 td {
